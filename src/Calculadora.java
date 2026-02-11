33e71a9ee1e14127b8778fac086e558d7806e065
nandaluvis <nandaoliveira.luvison@gmail.com>
2026-02-11 20:31:01 -0300

    Calculadora v1

---
+++ src/Calculadora.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Calculadora{
+
+public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bem vindo a calculadora!");
+        System.out.println("Digite um número de 1 a 4 para escolher a operação desejada de acordo com a tabela abaixo");
+        System.out.println("1 = + (Soma)");
+        System.out.println("2 = - (Subtração)");
+        System.out.println("3 = x (Multiplicação)");
+        System.out.println("4 = / (Divisão)");
+
+
+        System.out.println("Digite um número de 1 a 4");
+        int menu = scanner.nextInt();
+
+        System.out.println("Digite o primeiro número para realizar a operação");
+
+        double numeroEscolhidoUm = scanner.nextDouble();
+
+        System.out.println("Digite o segundo número para realizar a operação");
+
+        double numeroEscolhidoDois = scanner.nextDouble();
+
+
+        switch (menu){
+
+                case 1:
+                        System.out.println("O resultado da soma é " + (numeroEscolhidoUm + numeroEscolhidoDois));
+                        break;
+                case 2:;
+                        System.out.println("O resultado da subtração é " + (numeroEscolhidoUm - numeroEscolhidoDois));
+                        break;
+                case 3:;
+                        System.out.println("O resultado da multiplicação é " + (numeroEscolhidoUm * numeroEscolhidoDois));
+                        break;
+                case 4:;
+                        System.out.println("O resultado da divisão é " + (numeroEscolhidoUm / numeroEscolhidoDois));
+                        break;
+                default:
+                        System.out.println("Número inválido. Digite de um número de 1 a 4");
+        }
+
+}
+}
